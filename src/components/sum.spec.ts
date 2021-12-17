e92631e71c79cf3ef430e0dce55eb150a3e10550
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-12-17 21:45:15 +0000

    ⚡️ improving code performance with eslint
    checked

--- src/components/sum.spec.ts
+++ src/components/sum.spec.ts
@@ -3,6 +3,6 @@ import { sumNum } from "./sum";
 describe("sum the 2 number and return new value", () => {
   it("should return 9", () => {
     const value = sumNum(6, 3);
-    expect(value).toEqual(9);
+    expect(value).toBe(9);
   });
 });
