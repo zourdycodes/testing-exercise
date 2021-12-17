a8fd1c4d3f61aa24555f157ce08e6858106614dc
Muhammad Zourdy <zhafarmuhammad12@gmail.com>
2021-12-17 21:11:59 +0000

    🎨 setting up the test environment
    checked

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,19 @@
 
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
+  testEnvironment: "jsdom",
+  globals: {
+    "ts-jest": {
+      tsconfig: "./tsconfig.jest.json",
+    },
+  },
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
